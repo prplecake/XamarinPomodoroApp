9c8f54ecd9a60ec92190c17a0d2184c0f338f285
Matthew Jorgensen <matthew@jrgnsn.net>
2021-05-28 22:08:55 -0500

    Implement timer

--- Pomodoro/ViewController.cs
+++ Pomodoro/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Timers;
 using AppKit;
 using Foundation;
 
@@ -7,6 +7,10 @@ namespace Pomodoro
 {
     public partial class ViewController : NSViewController
     {
+
+        Timer MainTimer;
+        int TimeLeft = 1500; // 1500 seconds is 25 minutes
+
         public ViewController(IntPtr handle) : base(handle)
         {
         }
@@ -15,7 +19,57 @@ namespace Pomodoro
         {
             base.ViewDidLoad();
 
-            // Do any additional setup after loading the view.
+            // Fire the timer once a second
+            MainTimer = new Timer(1000);
+            MainTimer.Elapsed += (sender, e) =>
+            {
+                TimeLeft--;
+
+                // Format the remaining time nicely for the label
+                TimeSpan time = TimeSpan.FromSeconds(TimeLeft);
+                string timeString = time.ToString(@"mm\:ss");
+                InvokeOnMainThread(() =>
+                {
+                    // We want to interact with the UI from a different thread,
+                    // so we must invoke this change on the main thread
+                    TimerLabel.StringValue = timeString;
+                });
+
+                // If 25 minutes have passed
+                if (TimeLeft == 0)
+                {
+                    // Stop the timer and reset
+                    MainTimer.Stop();
+                    TimeLeft = 1500;
+                    InvokeOnMainThread(() =>
+                    {
+                        // Reset the UI
+                        TimerLabel.StringValue = "25:00";
+                        StartStopButton.Title = "Start";
+                        NSAlert alert = new NSAlert();
+                        // Set the style and message text
+                        alert.AlertStyle = NSAlertStyle.Informational;
+                        alert.MessageText = "25 minutes elasped! Take a 5 minute break.";
+                        // Display the NSAlert from the current view
+                        alert.BeginSheet(View.Window);
+                    });
+                }
+            };
+        }
+
+        partial void StartStopButtonClicked(NSObject sender)
+        {
+            // If the timer is running, we want to stop it,
+            // otherwise we want to start it
+            if(MainTimer.Enabled)
+            {
+                MainTimer.Stop();
+                StartStopButton.Title = "Start";
+            } else
+            {
+                MainTimer.Start();
+                StartStopButton.Title = "Stop";
+            }
         }
 
         public override NSObject RepresentedObject
